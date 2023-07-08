c96d5620aca77747ecd4eac348b8ccc4ce372347
Fernando Vega <58368168+normacocsub@users.noreply.github.com>
2023-07-08 10:34:41 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,23 +3,7 @@ import '../styles/globals.css'
 import { ROLES } from "../utils/constants";
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    const users = localStorage.getItem("usuarios")
-    if (users) {
-      return
-    }
-    const json = {
-      rol: ROLES.Admin,
-      correo: 'admin@gmail.com',
-      password: 'admi123',
-      cedula: '',
-      nombre: 'admin',
-      apellido: '',
-      direccion: '',
-      celular: ''
-    }
-    localStorage.setItem('usuarios', JSON.stringify([json]))
-  }, [])
+  
   return <Component {...pageProps} />
 }
 
